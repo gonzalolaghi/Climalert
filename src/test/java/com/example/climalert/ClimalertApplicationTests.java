b7a0955687845becb9dd2875ec558f8f0e9d29c3
gonzalolaghi <gsartorilaghi@frba.utn.edu.ar>
2026-07-02 22:49:52 -0300

    Commit inicial

---
+++ src/test/java/com/example/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
